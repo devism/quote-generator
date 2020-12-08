fc2fb953eef5254282756276f5069609c2dcb35f
oxvac <oxvac@bitbucket.org>
2020-12-08 15:34:45 -0800

    new commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import  {faTwitter} from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Anime, {anime} from 'react-anime';
+import data from './data/data.json';
 
+let quotes = data;
+
+let randomQuote = quotes.quotes[Math.floor(Math.random() * quotes.quotes.length)];
+
+// let animate = '360deg';
+let animate = '0deg';
 function App() {
+ 
+  const [quote, setQuote] = useState(randomQuote.quote);
+  const [quoteAuthor, setAuthor] = useState(randomQuote.author);
+  const [quoteImage, setImage] = useState(randomQuote.image);
+  // const [animate, setAnimate] = useState('0deg');
+
+  const newQuote = () => {
+    let randomQuote = quotes.quotes[Math.floor(Math.random() * quotes.quotes.length)];
+    setQuote(randomQuote.quote)
+    setAuthor(randomQuote.author)
+    setImage(randomQuote.image)
+
+    if(animate == '0deg'){
+      animate = '360deg';
+    } else {
+      animate = '0deg';
+    }
+
+    // setAnimate('360deg');
+    // console.log(animate);
+  }
+  
+  
+  const tweet = 'http://twitter.com/intent/tweet?text=' + encodeURIComponent(quote) + ' -' + encodeURIComponent(quoteAuthor);
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Anime translateY={'2em'} rotate={animate}>
+    <div id="quote-box" className="wrapper">
+      <div id="text"></div>
+      <p className="quote" id="quote">{quote}</p>
+      <p id="author"> - {quoteAuthor}</p>
+      <img src={quoteImage}/>
+      <div>
+        <button id="new-quote" className="new-quote-btn" onClick={newQuote}>
+          New Quote
+        </button>
+        
+        <a target="_blank" className="twitter-share-button" href={tweet} id="tweet-quote"> <FontAwesomeIcon icon={faTwitter} /> Tweet Quote</a>
+      </div>
+    </div> 
+    </Anime>
+    )
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import WebFont from 'webfontloader';
+
+WebFont.load({
+  google: {
+    families: ['Rubik', 'Jura', 'sans-serif']
+  }
+});
 
 ReactDOM.render(
   <React.StrictMode>
